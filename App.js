54eb2fcdad7079ae823d980571d46b39d93ee8a3
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-09-20 15:24:17 +0100

    navigating screens

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ChatScreen from './src/screens/ChatScreen';
+
+const App = () => {
+  const Stack = createNativeStackNavigator()
+  return (
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      <Stack.Screen name='ChatScreen' component={ChatScreen} />
+
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+};
+
+export default App;
