00fb633a3dcb453a47d2b733de12ba2874f0d1c5
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-09-21 02:11:53 +0100

    Navigating Screens edit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
 
 const HomeScreen = ({navigation}) => {
   return (
     <View>
-      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
-        <Button title='Continue' onPress={()=> navigation.navigate('ChatScreen')}/>
+      <Text style={{color:"black"}}>This is the Home screen</Text>
+        <Button title= "Continue" onPress={()=> navigation.navigate('ChatScreen')}/>
     </View>
   );
-}
+};
 
 export default HomeScreen;
 
