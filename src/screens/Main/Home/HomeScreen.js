18c05c27214070f2c31d221f9388c69a36fcef6f
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-10-02 16:04:22 +0100

    advanced flatlist usage complete

--- src/screens/Main/Home/HomeScreen.js
+++ src/screens/Main/Home/HomeScreen.js
@@ -1,7 +1,5 @@
 import { Image, StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
-import MiniButton from '../../../components/Button/MiniButton';
-import CartItem from '../../../components/Item/CartItem';
 
 const shirts = [
   {
@@ -9,31 +7,31 @@ const shirts = [
     title: 'Men T-Shirt',
     cost1: '$233',
     StarIcon: require('../../../assets/icons/icons8-star-100.png'),
-    Save: require('../../../assets/icons/icons8-briefcase-48.png'),
+    Save: require('../../../assets/icons/icons8-briefcase-100.png'),
   },
   {
     id: 2, picture: require('../../../assets/images/22.png'),
     title: 'Men T-Shirt',
     cost1: '$233',
     StarIcon: require('../../../assets/icons/icons8-star-100.png'),
-    Save: require('../../../assets/icons/icons8-briefcase-48.png'),
+    Save: require('../../../assets/icons/icons8-briefcase-100.png'),
   },
   {
     id: 3, picture: require('../../../assets/images/33.jpg'),
-    title: 'Men T-Shirt',
+    title: 'Women T-Shirt',
     cost1: '$233',
     StarIcon: require('../../../assets/icons/icons8-star-100.png'),
-    Save: require('../../../assets/icons/icons8-briefcase-48.png'),
+    Save: require('../../../assets/icons/icons8-briefcase-100.png'),
   },
   {
     id: 4, picture: require('../../../assets/images/33.jpg'),
     title: 'Men T-Shirt',
     cost1: '$233',
     StarIcon: require('../../../assets/icons/icons8-star-100.png'),
-    Save: require('../../../assets/icons/icons8-briefcase-48.png'),
+    Save: require('../../../assets/icons/icons8-briefcase-100.png'),
   },
 
-]
+];
 
 const HomeScreen = ({ navigation }) => {
 
@@ -73,9 +71,30 @@ const HomeScreen = ({ navigation }) => {
             }} />
         </View>
 
-        <View>
+        <View style={{ marginBottom: 19 }}>
 
-          <FlatList />
+          <FlatList data={shirts}
+            numColumns={2}
+            columnWrapperStyle={{ justifyContent: 'space-between' }}
+            renderItem={({ item }) => {
+              return (
+                <View style={{ marginTop: 10, }}>
+                  <Image source={item.picture} style={{ height: 172, width: 149, }} />
+                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 8 }}>
+                    <Text style={{ fontSize: 13, fontWeight: '600', color: 'black' }}>{item.title}</Text>
+                    <Text style={{ fontSize: 13, fontWeight: '600', color: '#BB5D5C' }}>{item.cost1}</Text>
+                  </View>
+                  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 8 }}>
+                    <TouchableOpacity>
+                      <Image source={item.StarIcon} style={{ height: 20, width: 21 }} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ backgroundColor: '#BB5D5C', width: 30, height: 27, alignItems: 'center', justifyContent: 'center', borderRadius: 20 }}>
+                      <Image source={item.Save} style={{ height: 20, width: 21 }} />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )
+            }} />
         </View>
       </View>
 
@@ -95,20 +114,26 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.page}>
 
-      <View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+        <View style={{}}>
+          <Text style={{ fontWeight: 'bold', fontSize: 17, color: '#BB5D5C' }}>Welcome Back!</Text>
+          <Text style={{ fontWeight: '600', fontSize: 12, color: 'black', textAlign: 'center' }}>Ayanyiola Enoch</Text>
 
+        </View>
+        <TouchableOpacity>
+          <Image source={require('../../../assets/icons/icons8-notification-100.png')} style={{ width: 20, height: 20 }} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ marginTop: 19 }}>
+        <Image source={require('../../../assets/icons/icons8-search-100.png')} style={{ width: 20, height: 20, position: 'absolute', top: 9, left: 10 }} />
+        <TextInput placeholder='Search' style={{ width: 320, height: 40, borderWidth: 2, borderRadius: 10, paddingHorizontal: 30, }} />
+        <Image source={require('../../../assets/icons/icons8-microphone-100.png')} style={{ width: 20, height: 20, position: 'absolute', top: 9, right: 10 }} />
       </View>
 
       <FlatList
         data={['']}
         ListHeaderComponent={RenderHeader} />
-
-
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <CartItem title={'Men T-shirt'} price={'S233'} navigation={navigation} />
-        <CartItem title={'Men T-shirt'} price={'S233'} />
-      </View>
 
     </View>
   );
@@ -122,10 +147,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     backgroundColor: '#ffffff',
     flex: 1,
-  },
-  notificationIcon: {
-    width: 20,
-    height: 20
   },
   input: {
     marginTop: 20,
