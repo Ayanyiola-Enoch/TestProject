b60ca68f239feb7784d55b5e9e8abc04f62d4bb2
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-10-20 16:54:51 +0100

    problem on building the app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-const Header = ({header}) => {
+const Header = ({ header }) => {
   return (
-    <View style={{marginTop:18, flexDirection:'row', alignItems:'center', marginBottom:20 }}>
-        <View style ={{width:49, height:46, backgroundColor:'#D8D8D8', justifyContent:'center', alignItems:'center'}}>
-            <Image source={require('../../assets/icons/icons8-back-24.png')} style={{width:25, height:25}}/>      
-        </View>
-        <Text style={{marginLeft:10, fontSize:17, fontWeight:'bold', color:'black'}}>{header}</Text>
+    <View style={{ marginTop: 18, flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+      <View style={{ width: 49, height: 46, backgroundColor: '#D8D8D8', justifyContent: 'center', alignItems: 'center' }}>
+        <Image source={require('../../assets/icons/icons8-back-24.png')} style={{ width: 25, height: 25 }} />
+      </View>
+      <Text style={{ marginLeft: 10, fontSize: 17, fontWeight: 'bold', color: 'black' }}>{header}</Text>
     </View>
   );
 };
@@ -15,4 +15,5 @@ const Header = ({header}) => {
 export default Header;
 
 const styles = StyleSheet.create({
+
 });
